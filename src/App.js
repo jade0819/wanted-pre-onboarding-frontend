e052401bfba343a5178924b8b09971e7e45b9c49
jade0819 <break0819@gmail.com>
2023-06-18 16:31:42 +0900

    Chore: Add react-router-dom@6 and page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TodoList from "./pages/TodoList";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
