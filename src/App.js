ec9a027235965ae61df0dcee83d0d4c96c8da791
jade0819 <break0819@gmail.com>
2023-08-06 10:06:20 +0900

    Feat: 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/signin/SignInPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import NotFoundPage from "./pages/notfound/NotFoundPage";
+import TodoPage from "./pages/todo/TodoPage";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/signup/SignUpPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignUpPage() {
+  return <div>회원가입</div>;
+}
