1ddd9e6b4fa6bff32ea4f782c6d68b28bf9be81a
jade0819 <break0819@gmail.com>
2023-08-06 13:32:46 +0900

    Design: 투두리스트 UI 생성

--- src/pages/todo/TodoPage.jsx
+++ src/pages/todo/TodoPage.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useAuthContext } from "../../context/AuthContext";
 
-export default function TodoPage() {
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+
+  const { accessToken: token } = useAuthContext();
+
   const { removeToken } = useAuthContext();
 
+  const handleClick = () => {
+    removeToken();
+  };
+
+  const getTodos = useCallback(() => {
+    if (token) {
+      setTodos("성공!!!!");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    getTodos();
+  }, [getTodos]);
+
   return (
-    <div>
-      투두리스트
-      <button onClick={removeToken}>로그아웃</button>
+    <div className="w-full max-w-[600px] justify-center px-1">
+      <div className="text-right mb-1">
+        <button onClick={handleClick}>로그아웃</button>
+      </div>
+
+      <div className="w-full h-[520px] p-4 bg-white">{todos}</div>
     </div>
   );
 }
