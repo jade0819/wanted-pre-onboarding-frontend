2422fd572384f61257b93e9ac263bbb059c80a2f
jade0819 <break0819@gmail.com>
2023-08-06 10:22:52 +0900

    Design: 전체 페이지 스타일 수정

--- src/pages/signin/SignInPage.jsx
+++ src/pages/signin/SignInPage.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignInPage() {
-  return <div className="text-red-600">로그인</div>;
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className="w-full max-w-lg">
+      <div className="text-3xl font-semibold text-center mb-6">로그인</div>
+      <form className="p-1" onSubmit={handleSubmit}>
+        <div className="flex justify-between items-center">
+          <input
+            id="email"
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <input
+            id="password"
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+          <button type="submit">로그인</button>
+        </div>
+      </form>
+    </div>
+  );
 }
