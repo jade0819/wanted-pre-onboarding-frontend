9da4ca4d600083934d700af132e7d41a07aaf39b
jade0819 <break0819@gmail.com>
2024-04-30 18:34:20 +0900

    Refactor: httpClient 클래스에 baseURL과 storage(토큰 저장)에 대한 의존성 주입

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HttpClient } from "./apis/httpClient";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const httpClient = new HttpClient(
+  "https://www.pre-onboarding-selection-task.store",
+  "storage"
+);
+
 root.render(
   <React.StrictMode>
     <App />
