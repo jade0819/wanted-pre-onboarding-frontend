adf2184a78d4c7945ce20b70ed21a19f2bb0fb73
jade0819 <break0819@gmail.com>
2024-04-30 18:41:32 +0900

    Feat: 로컬 스토리지에 인증 토큰을 저장하기 위해 스토리지 모듈 관심사 분리

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { HttpClient } from "./apis/httpClient";
+import { LocalStorage } from "./storage/localStorage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const storage = new LocalStorage();
 const httpClient = new HttpClient(
   "https://www.pre-onboarding-selection-task.store",
-  "storage"
+  storage
 );
 
 root.render(
